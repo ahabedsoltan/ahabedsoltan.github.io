b81029e943fa070294a68bd747020c8bc3239e37
Amirhesam Abedsoltan <91305454+ahabedsoltan@users.noreply.github.com>
2025-09-24 17:51:08 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',          // required for static export
+  output: 'export',            // required for static export
   images: { unoptimized: true } // avoids Next image optimizer
   // optional: trailingSlash: true
 };
+
 export default nextConfig;
